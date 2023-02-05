0172e15f0cb74b3c169702bcf9362f474a5edb5b
routinues <routinues@gmail.com>
2023-02-05 23:09:57 +0300

    Constructors eklendi.

--- Constructors/Properties/AssemblyInfo.cs
+++ Constructors/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReferenceTypes")]
+[assembly: AssemblyTitle("Constructors")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReferenceTypes")]
+[assembly: AssemblyProduct("Constructors")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b216fd59-27f4-4717-8e76-c371b25b0f39")]
+[assembly: Guid("0830e816-db44-4993-951d-946d5f247329")]
 
 // Version information for an assembly consists of the following four values:
 //
